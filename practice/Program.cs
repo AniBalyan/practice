01a5f46f8776fc7d1255b839f45d56d450b8a219
AniBalyan <AniBalyan@LAPTOP-LCE2EEK2>
2023-04-25 22:26:10 +0400

    Add project files.

---
+++ practice/Program.cs
@@ -0,0 +1,26 @@
+﻿/* int x = 4321;
+int x1 = x%10; //1
+x = x/10; //432
+int x2 = x%10; //2
+x=x/10; //43
+int x3 = x%10; //3
+int x4 =x/10; //4
+int sum = x1 + x2 + x3 +x4; 
+Console.WriteLine(sum); */
+
+int x = 4322;
+int x1;
+int sum = 0;
+
+while (x > 0)
+{
+    x1 = x%10;
+    x/=10;
+    sum = sum + x1;
+    Console.WriteLine(x1);
+
+}
+Console.WriteLine("SUM: " + sum);
+
+
+
